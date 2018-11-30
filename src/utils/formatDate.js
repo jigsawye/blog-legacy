923228c2eba74ce392f5315c815d08f136464bb0
jigsawye <jigsaw.ye@gmail.com>
2018-11-30 11:08:48 +0800

    fix timezone

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,14 +1,17 @@
 import TimeAgo from 'javascript-time-ago';
 import zhHant from 'javascript-time-ago/locale/zh-Hant';
 
-TimeAgo.locale(zhHant);
+TimeAgo.addLocale(zhHant);
 const timeAgo = new TimeAgo('zh-Hant');
 
 export default date => {
   const d = new Date(date);
+  d.setTime(d.getTime() + d.getTimezoneOffset() * 60 * 1000);
+
   const year = d.getFullYear();
   const month = d.getMonth() + 1;
   const day = d.getDate();
+
   const time = timeAgo.format(d);
 
   return `${year} 年 ${month} 月 ${day} 日（${time}）`;
